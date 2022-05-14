b60132eb88a693f2d3467297bd85ddf820ccb487
Jooyul Yoon <juyeolyoon@gmail.com>
2022-05-14 03:55:27 +0000

    v1.1 Routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
