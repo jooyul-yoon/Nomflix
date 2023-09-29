77b71dfbfda40144efa98bead09c27c0ab8e10df
Jooyul Yoon <juyeolyoon@gmail.com>
2023-09-29 15:02:43 -0700

    genre data api added

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,5 @@
 const API_KEY = "79237aa8c0758e58f65a9c70bbc66c18";
 const BASE_URL = "https://api.themoviedb.org/3/";
-// const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
-
-// type imageType = "original" | "w500";
 
 interface IMovie {
   id: number;
@@ -45,4 +42,14 @@ export function getUpcomingMovies() {
   return fetch(`${BASE_URL}movie/upcoming?api_key=${API_KEY}`).then(
     (response) => response.json()
   );
+}
+
+export function getGenreData() {
+  return fetch(`${BASE_URL}genre/movie/list`, {
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTIzN2FhOGMwNzU4ZTU4ZjY1YTljNzBiYmM2NmMxOCIsInN1YiI6IjYxZTQyNjA5MGNkMzJhMDA5ZGU1MTAzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lL55mrv6DWQA8waZg14tBigsilqs9NfE1zRxohf8Ms4",
+    },
+  }).then((response) => response.json());
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
-export function makeImagePath(id: string, format?:string){
-    return `https://image.tmdb.org/t/p/${format?format:"original"}/${id}`;
+export function makeImagePath(
+  id: string,
+  format?: "original" | "w500" | "w200"
+) {
+  return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
+}
+
+export function getGenreById(
+  genres: { id: number; name: string }[],
+  id: number
+) {
+  return genres.find((data) => data.id === id)?.name;
 }
